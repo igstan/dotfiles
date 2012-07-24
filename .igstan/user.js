5823a73d77f994d8bb6aa59a49fef0654361509d
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-07-24 11:20:35 +0300

    Create Firefox user preferences file

---
+++ .igstan/user.js
@@ -0,0 +1,20 @@
+//
+// Firefox user preferences file
+//
+
+user_pref("accessibility.warn_on_browsewithcaret", false);
+user_pref("browser.altClickSave", true);
+user_pref("browser.feeds.showFirstRunUI", false);
+user_pref("browser.gesture.swipe.down", "cmd_scrollTop");
+user_pref("browser.gesture.swipe.up", "cmd_scrollBottom");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.panorama.animate_zoom", false);
+user_pref("browser.panorama.experienced_first_run", true);
+user_pref("browser.places.smartBookmarksVersion", 4);
+user_pref("browser.search.openintab", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.startup.homepage", "http://www.google.com/");
+user_pref("browser.tabs.closeButtons", 2);
+user_pref("dom.disable_window_open_feature.scrollbars", true);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("privacy.donottrackheader.enabled", true);
