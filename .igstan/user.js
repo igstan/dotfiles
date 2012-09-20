19e7356cd337c0f6a1e68c21aa0e4c3d98da46d3
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-09-20 12:34:33 +0300

    Remove Firefox preference in user.js file

--- .igstan/user.js
+++ .igstan/user.js
@@ -10,7 +10,6 @@ user_pref("browser.gesture.swipe.up", "cmd_scrollBottom");
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.panorama.animate_zoom", false);
 user_pref("browser.panorama.experienced_first_run", true);
-user_pref("browser.places.smartBookmarksVersion", 4);
 user_pref("browser.search.openintab", true);
 user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.startup.homepage", "http://www.google.com/");
@@ -18,3 +17,6 @@ user_pref("browser.tabs.closeButtons", 2);
 user_pref("dom.disable_window_open_feature.scrollbars", true);
 user_pref("general.warnOnAboutConfig", false);
 user_pref("privacy.donottrackheader.enabled", true);
+
+// warn on quit if downloads are in progress
+user_pref("browser.download.manager.quitBehavior", 2);
